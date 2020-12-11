4df7472a30d127b4713b5f5a2e96a3187ee7826a
N0star <64681977+N0star@users.noreply.github.com>
2020-12-11 20:08:09 +0100

    multicommand
    now multiple commands can be executed with just one input.

--- pxc.py
+++ pxc.py
@@ -9,6 +9,7 @@ def upload(name):           #get an image
     for i in range(len(img)):
         for j in range(len(img[i])):
             tab.append(img[i][j])
+    print("table size: ",len(tab))
     return tab, img
 
 def minipix(xx,cc,idx):     #reduce the colours
@@ -29,7 +30,7 @@ def download(xx,pic):       #prepare to save
     return pic
 
 
-def kc_init(xx, k):         #init k-index centroids
+def kc_init(xx, k):         #init k-mean centroids
     ridx = np.random.permutation(len(xx))
     ctrd = xx[:k]
     return ctrd
@@ -65,51 +66,96 @@ def ccc(xx,idx,k):          #update centroids
         cc[i]/=c[i]
     return cc
 
-def setting():
-    name = input("Image name: ")
-    tab = []; pic = [];
-    f = 0; n = 0; k = 0;
-    try:
-        tab,pic = upload(name)
-    except FileNotFoundError: f=1;
-    print("table size: ",len(tab))
-    n = input("Number of interations (int): ")
-    k = input("Number of colours (int): ")
+def setting(n=-1,k=-1):
+    if(n<0): n = input("Number of interations (int): ")
+    if(k<0): k = input("Number of colours (int): ")
     n = int(n); k = int(k)%256;
     print("File: ",name,", k=",k,", n=",n)
     ch = input("Is this data correct? (Y/n): ")
-    if(ch=='Y' or ch=='y'):
-        if(f): print("File not found...")
-        else: pass
+    if(ch=='Y' or ch=='y'): f=0;    
     else: f=1;
     return f,tab,pic,n,k;
 
 #tab, pic = upload("Homare.jpeg")
 #k=8; tab = tab[:1000] # testing
+tab = []; pic = []; img = False;
+
 print("Welcome to Pixacolor!")
 while True:                         # main menu # MAIN LOOP #
-    ch=input("\nu - upload, s - save,\ni - info, q - quit.\n\n")
+    ch=input("\nu - upload, s - save,\nr - reduce colors\n"
+             +"i - info, q - quit.\n\n"); cmd=ch.split(); ch=ch[0];
+    
+    upl = 0; red = 0; sav = 0; inf = 0; qui = 0;
+    for i in range(len(cmd)):
+        if(cmd[i]=='u'):         #case upload
+            upl+=1;
+            try:
+                name=cmd[i+1]   #req img name
+                upl+=1; j=0;
+                try:
+                    if(cmd[i+2]=='r'): j=1;
+                    n=int(cmd[i+2+j]);
+                    k=int(cmd[i+3+j]);
+                    upl+=1; red=2;
+                except IndexError: continue
+            except IndexError: continue
+            
+        elif(cmd[i]=='r'):       #case reduce
+            red=1;
+            try:
+                n=int(cmd[i+1]);
+                k=int(cmd[i+2]);
+                red=2;
+            except IndexError: continue
+            except ValueError: continue
+
+        elif(cmd[i]=='s' or cmd[i]=='as'): #case save
+            if(cmd[i]=='as'):
+                target=name+"_"+str(k)+"x"+str(n);
+                sav=2;
+            else:
+                sav=1;
+                try:
+                    target=cmd[i+1]; sav=2;
+                except IndexError: continue
+
+        elif(cmd[i]=='i' ): inf=1; #case info
+        elif(cmd[i]=='q' ): qui=1; #case quit
+            
     if(ch=='q'): break;
-    elif(ch=='u'):
-        f,tab,pic,itn,k = setting()
-        if(not f):
-            cc = kc_init(tab,k)
-            for i in range(itn):
-                print("Searching...",i+1)
+    if(upl>0):
+        if(upl<2):
+            name = input("Image name: ")
+        tab = []; pic = []; img = True;
+        try:
+            tab,pic = upload(name)
+        except FileNotFoundError:
+            img=False; print("File not found...");
+    if(red>0 or upl==3):
+        if(img):
+            if(red<2): f,tab,pic,itn,k = setting()
+            else: f,tab,pic,itn,k = setting(n,k)
+            if(not f):
+                cc = kc_init(tab,k)
+                for i in range(itn):
+                    print("Searching...",i+1)
+                    idx = find_cc(tab,cc)
+                    print("Updating...",i+1)
+                    cc = ccc(tab,idx,k)
+                print("Final search...")
                 idx = find_cc(tab,cc)
-                print("Updating...",i+1)
-                cc = ccc(tab,idx,k)
-            print("Final search...")
-            idx = find_cc(tab,cc)
-            print("Reducing...")
-            xx = minipix(tab,cc,idx)
-            print("Completing...")
-            npic = download(xx,pic)
-            print("Ready!")
-    elif(ch=='s'):
+                print("Reducing...")
+                xx = minipix(tab,cc,idx)
+                print("Completing...")
+                npic = download(xx,pic)
+                print("Ready!")
+        else: print("No image's been found!");
+    if(sav>0):
         if(npic is not None):
-            print("Name file to save, or...\ntype 'show' to show instead")
-            name=input()
+            if(sav<2):
+                print("Name file to save, or...\ntype 'show' to show instead")
+                name=input()
+            else: name=target;
             fig = plt.matshow(npic)
             if(name=='show'):
                 plt.show()
@@ -118,5 +164,6 @@ while True:                         # main menu # MAIN LOOP #
                 plt.axis('off')
                 plt.savefig(name, bbox_inches='tight')
         else: print("No image to save")
-    elif(ch=='i'):
+    if(inf):
         print(I);
+    if(qui): break;
